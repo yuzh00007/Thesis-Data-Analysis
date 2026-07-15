3dbcfae672b30d93eb54523681836f5c645d5d7e
yzh00007 <yzhang3283@gmail.com>
2026-07-15 11:23:34 +0200

    updated all priors and ran model, uploading new outputs

--- modeling/BMRS/thesisModel.R
+++ modeling/BMRS/thesisModel.R
@@ -11,15 +11,32 @@ library(dplyr)
 results <- read.csv("./formatted_results.csv")
 set.seed(123)
 
-# FORMULA 
+# print(
+#   get_prior(
+#     derivationStrength ~ speakerType + speakerGender + nathan_score_full + freqRatio + itemType
+#     + speakerType * itemType
+#     + speakerType * freqRatio
+#     + speakerType * speakerGender
+#     + speakerType * nathan_score_full
+#     + itemType * nathan_score_full
+#     + speakerType * nathan_score_full * itemType
+#     + speakerType * freqRatio * itemType
+#     + (1|itemId)
+#     + (1+speakerType+speakerGender|participantId),
+#     data=results, family=brmsfamily("normal", "log")
+#   ),
+#   options("width"=2000)
+# )
+
+# FORMULA
 formula1 <- brmsformula(
-  derivationStrength ~ speakerType + speakerGender + ToM + freqRatio + itemType
+  derivationStrength ~ speakerType + speakerGender + nathan_score_full + freqRatio + itemType
   + speakerType * itemType
   + speakerType * freqRatio
   + speakerType * speakerGender
-  + speakerType * ToM
-  + itemType * ToM
-  + speakerType * ToM * itemType
+  + speakerType * nathan_score_full
+  + itemType * nathan_score_full
+  + speakerType * nathan_score_full * itemType
   + speakerType * freqRatio * itemType
   + (1|itemId)
   + (1+speakerType+speakerGender|participantId)
@@ -27,10 +44,21 @@ formula1 <- brmsformula(
 
 # PRIORS
 model1_priors <- c(
+  # fixed effects
   prior(normal(87.5, 12.5), class = "Intercept"),
-  prior(normal(0, 12.5), class = "speakerType"),
-  # prior(lognormal(4.2, .3), class="Intercept"),
-  prior(normal(0, 1), class = "b"),
+  prior(normal(0, 5), class = "b"),
+  prior(normal(0, 12.5), class = "b", coef="speakerTypenonnative"),
+  prior(normal(-25, 12.5), class = "b", coef="itemTypeunambiguous"),
+  prior(normal(0, 12.5), class = "b", coef="speakerTypenonnative:itemTypeunambiguous"),
+
+  # random effects
+  prior(student_t(3, 0, 12.5), class = "sd", group="itemId"),
+  prior(student_t(3, 0, 12.5), class = "sd", group="participantId"),
+
+  # residual standard deviation?
+  prior(student_t(3, 0, 2.5), class = "sd"),
+
+  # correlation structure
   prior_string("lkj(2)", class = "cor")
 )
 
@@ -40,8 +68,9 @@ model1 <- brm(
   formula1,
   data = results,
   family = gaussian(),
-  prior = model1_priors, 
+  prior = model1_priors,
   chains = 4,
+  warmup=1000,
   iter = 4000,
   seed = 123
 )
